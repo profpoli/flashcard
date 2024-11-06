7b6a8945f6a73ade9fe74676fac131dce7f92f21
profpoli <102968293+profpoli@users.noreply.github.com>
2024-11-05 21:08:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação de hipertexto utilizada na criação de documentos e páginas da web. '
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o cubo de 5?',
+    'O cubo de 5 é 125'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que é paradoxo?',
+    'É uma declaração aparentemente verdadeira, mas que leva a uma contradição lógica ou que contradiz a intuição comum. '
+)
+criaCartao(
+    'Matemática',
+    'O que é um triângulo retângulo?',
+    'É um triângulo que possui um ângulo reto'
+)
+criaCartao(
+    'Programação',
+    'O que é javascript?',
+    'JavaScript é uma linguagem de programação que permite a criação de páginas web interativas, com conteúdo dinâmico, gráficos animados, mapas interativos, entre outros. '
+)
